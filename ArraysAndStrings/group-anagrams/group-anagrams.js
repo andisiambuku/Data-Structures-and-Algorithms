a8e0efdc0d7c67f08e2e0d2d71c1c5b0745f9053
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-10-30 16:52:48 +0300

    group anagrams question

---
+++ ArraysAndStrings/group-anagrams/group-anagrams.js
@@ -0,0 +1,21 @@
+const groupAnagrams = strs => {
+	let results = {}
+
+	const sortWord = word => {
+		return word
+			.split('')
+			.sort((a, b) => (a < b ? -1 : 1))
+			.join('')
+	}
+
+	for (let i = 0; i < strs.length; i++) {
+		let sortedWord = sortWord(strs[i])
+
+		if (results[sortedWord]) {
+			results[sortedWord].push(strs[i])
+		} else {
+			results[sortedWord] = [strs[i]]
+		}
+	}
+	return Object.values(results)
+}
