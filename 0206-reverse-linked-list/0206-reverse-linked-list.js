e075cf06a64f72b58e1416bf3f74294d2148ddb2
Andisi Ambuku <66791412+andisiambuku@users.noreply.github.com>
2023-04-10 22:08:05 +0300

    Time: 58 ms (87.47%), Space: 44.4 MB (36.94%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,87 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+* @param {ListNode} head
+
+ * @return {ListNode}
+ */
+var reverseList = function(head,previous=null) {
+    
+    let curr = head
+    let prev = null
+    
+    while(curr !== null){
+        let next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    } 
+    return prev;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    // how 
+    /*
+    Iterative method to reverse linked list
+    a method takes in the next node
+    swaps it with the current node
+    the new ne
+    */
+
+    // let current = head
+    // let previous = null
+    // let next = null
+
+    // while(current != null){
+    //     next = current.next
+    //     current.next = previous
+    //     previous = current
+    //     current = next
+        
+    // }
+    // return previous
+
+    // Space complexity => O(n)
+    // Time Complexity => O(n)
+
+   
+     if(!head) return previous
+    
+    let next = head.next
+    
+    head.next = previous
+    return reverseList(next,head)
+
+
+
+
+
+
+
+
+};
