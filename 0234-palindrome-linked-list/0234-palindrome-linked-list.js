fe19dffd6bd0a6809f955b8327e1662c157a1df6
Andisi Ambuku <66791412+andisiambuku@users.noreply.github.com>
2023-04-21 21:45:01 +0300

    Time: 145 ms (84.44%), Space: 63.3 MB (75.16%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,16 +10,45 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    let slow = head, fast = head, prev, temp
-    while (fast && fast.next)
-        slow = slow.next, fast = fast.next.next
-    prev = slow, slow = slow.next, prev.next = null
-    while (slow)
-        temp = slow.next, slow.next = prev, prev = slow, slow = temp
-    fast = head, slow = prev
-    while (slow)
-        if (fast.val !== slow.val) return false
-        else fast = fast.next, slow = slow.next
-    return true
+    if(head.next === null || head === null) return true
     
+   let slow = head
+   let fast = head
+   
+   
+   //condition for the hare
+   while(fast && fast.next){
+       slow = slow.next
+       fast = fast.next.next 
+       // prev = slow
+       // prev.next = null    
+   }
+    //condition for the turtle
+    let prev = null
+    while(slow){
+        let temp = slow.next
+        slow.next = prev
+        prev = slow
+        slow = temp       
+    }
+    // console.log(slow)
+    // console.log(fast)
+    //condition for the comparison
+    let first = head, last = prev
+    // console.log(last.val)
+    // console.log(last.next.val)
+    
+    while(last){
+        if(first.val !== last.val){
+            return false
+        } 
+        first = first.next
+        last = last.next          
+    }
+    return true
+   
+   
+   
+   
+   
 };
