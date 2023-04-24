cc7585f870d19b460f4e24c381a6ce8598f18fed
Andisi Ambuku <66791412+andisiambuku@users.noreply.github.com>
2023-04-24 22:58:26 +0300

    Time: 63 ms (72.50%), Space: 44.2 MB (74.26%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.js
@@ -27,4 +27,31 @@ let itr = current.next;
 while(itr != null && itr.val == current.val) itr = itr.next; // 
 return deleteDuplicates(itr);
     
+    //iterative approach
+//     const dummy = new ListNode(0, head)
+//     let current = head
+//     let prev = dummy
+//     // 1 -> 1 -> 1 -> 2 -> 3
+//     //                c 
+// // prev {0, next}
+// // dummy {0, next}
+// // prev.next = 2 -> 3
+// //.                           prev
+//     // 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
+//     //                               c
+
+//     while(current){
+//         if(current && current.next && current.val === current.next.val){
+//             while (current.next && current.val === current.next.val){
+//                 current = current.next
+//             }
+//             current = current.next
+//             prev.next = current
+//         }else{
+//             current = current.next
+//             prev = prev.next
+//         }   
+//     }
+//     return dummy.next
+    
 };
