5897863aab03787a8d5ab9851e6d21a8d510dcbe
Andisi Ambuku <66791412+andisiambuku@users.noreply.github.com>
2023-05-26 21:40:42 +0300

    Time: 155 ms (95.49%), Space: 50.2 MB (93.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,20 +3,33 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-   if(x<0){
+    //if less than 0
+    if(x<0){
        return false
-   }
+       }
+    
+    //less than 10 but more than 0
     if(x<10){
         return true
     }
+    // splitting to individual digits
     let xSplit = x.toString().split("")
     
-    for(let i=0; i< xSplit.length; i++){
-        if (xSplit[i] != xSplit[xSplit.length-i-1]){
+    //
+    for(let i=0; i<xSplit.length; i++){
+        //comparing the first and last digit 
+        if(xSplit[i] != xSplit[xSplit.length -i -1]){
+            
             return false
         }
     }
-    return true
-}
+    return true       }
 
+// 1 -> 2 -> 1 = 121
+// 1 <- 2 <- 1 = 121 Palindrome
 
+// -1 -> 2 -> 1 = -121
+//  -1<- 2<- 1 = 121- Not Palindrome
+
+// 1 -> 0 = 10 
+// 1 <- 0 = 01 Not Palindrome
