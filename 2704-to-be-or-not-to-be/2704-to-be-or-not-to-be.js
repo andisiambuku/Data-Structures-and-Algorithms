4c4e3122f6407a02f090764d47e5aa6f08c20e91
Andisi Ambuku <66791412+andisiambuku@users.noreply.github.com>
2023-06-02 21:25:17 +0300

    Time: 57 ms (65.18%), Space: 41.9 MB (43.42%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -4,14 +4,16 @@
  */
 var expect = function(val) {
     return {
-        toBe: (val2) => {
-            if (val !== val2) throw new Error("Not Equal");
-            else return true;
+        toBe: (valTest) =>{
+            if(val !== valTest) throw new Error("Not Equal");
+            else return true
         },
-        notToBe: (val2) => {
-            if (val === val2) throw new Error("Equal");
-            else return true;
+        
+        notToBe: (valTest) =>{
+            if(val === valTest) throw new Error("Equal");
+            else return true
         }
+       
     }
 };
 
