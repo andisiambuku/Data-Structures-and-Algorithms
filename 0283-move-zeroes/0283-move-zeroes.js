1d3b1c4e329ebbf436c6f37a2ac84b8a19cf9d0a
Andisi Ambuku <66791412+andisiambuku@users.noreply.github.com>
2023-09-30 10:47:02 +0300

    Time: 76 ms (59.48%), Space: 46.1 MB (90.10%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,22 +3,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let nonZeroIdx = 0; // Points to the next position where a non-zero element should be placed
-
-    // Iterate through the array
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            // If the current element is non-zero, move it to the nonZeroIdx position
-            nums[nonZeroIdx] = nums[i];
-
-            // If the current element was not at the nonZeroIdx position, set it to 0
-            if (nonZeroIdx !== i) {
-                nums[i] = 0;
-            }
-
-            // Increment nonZeroIdx to the next available position
-            nonZeroIdx++;
-        }
+    let z = 0
+    
+    for(i=0; i<nums.length; i++){
+        
+        if(nums[i] !== 0){
+            
+           nums[z] = nums[i]
+            
+            if(z !== i){
+                
+               nums[i] = 0
+               }
+            z++
+           }
     }
     
 };
